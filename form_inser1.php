8dbd83b487f1cd23fe47872c0203b4577ca37a6c
gapsurat <gapsurat@gmail.com>
2017-08-16 01:55:30 +0700

    insert 1

---
+++ form_inser1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Unbenanntes Dokument</title>
+</head>
+
+<body>
+<form id="form_register" name="form_register" method="post" action="">
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <table width="62%" border="0" align="center">
+    <tr>
+      <td height="46" colspan="2" align="center" bgcolor="#D6D6D6">Register</td>
+    </tr>
+    <tr>
+      <td width="23%" align="right" bgcolor="#D6D6D6">Username</td>
+      <td width="77%" bgcolor="#D6D6D6"><label for="username"></label>
+      <input type="text" name="username" id="username" /></td>
+    </tr>
+    <tr>
+      <td align="right" bgcolor="#D6D6D6">Password</td>
+      <td bgcolor="#D6D6D6"><label for="password"></label>
+      <input type="text" name="password" id="password" /></td>
+    </tr>
+    <tr>
+      <td align="right" bgcolor="#D6D6D6">Name</td>
+      <td bgcolor="#D6D6D6"><label for="name"></label>
+      <input type="text" name="name" id="name" /></td>
+    </tr>
+    <tr>
+      <td align="right" bgcolor="#D6D6D6">&nbsp;</td>
+      <td bgcolor="#D6D6D6">&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right" bgcolor="#D6D6D6">&nbsp;</td>
+      <td bgcolor="#D6D6D6"><input type="submit" name="save" id="save" value="Submit" /></td>
+    </tr>
+  </table>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+</form>
+</body>
+</html>
