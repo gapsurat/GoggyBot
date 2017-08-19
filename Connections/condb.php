ba2230e0828c679551040786430572204d0e68bd
gapsurat <gapsurat@gmail.com>
2017-08-19 17:50:57 +0700

    Deploy

--- Connections/condb.php
+++ Connections/condb.php
@@ -2,10 +2,10 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_condb = "localhost:";
+$hostname_condb = "ap-cdbr-azure-southeast-b.cloudapp.net";
 $database_condb = "goggybot";
-$username_condb = "root";
-$password_condb = "root";
+$username_condb = "bad690fe6d0cb7";
+$password_condb = "bd8c31ba";
 $condb = mysql_pconnect($hostname_condb, $username_condb, $password_condb) or trigger_error(mysql_error(),E_USER_ERROR); 
 mysql_query("SET NAMES UTF8",$condb);	
 ?>
