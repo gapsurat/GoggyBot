d7bef9bea06cac22661569f27a1fe811a5a3bf19
gapsurat <gapsurat@gmail.com>
2017-08-20 09:33:35 +0700

    Update condb.php

--- Connections/condb.php
+++ Connections/condb.php
@@ -2,10 +2,10 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_condb = "ap-cdbr-azure-southeast-b.cloudapp.net";
+$hostname_condb = "";
 $database_condb = "goggybot";
-$username_condb = "bad690fe6d0cb7";
-$password_condb = "bd8c31ba";
+$username_condb = "";
+$password_condb = "";
 $condb = mysql_pconnect($hostname_condb, $username_condb, $password_condb) or trigger_error(mysql_error(),E_USER_ERROR); 
 mysql_query("SET NAMES UTF8",$condb);	
 ?>
